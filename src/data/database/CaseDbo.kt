f8ae387c8f7ebf0d71f31a30de833b69975ab5fb
ilya-shevtsov <Dayzilya@gmail.com>
2021-04-11 17:16:57 +0300

    Added: - insertToDatabase method - refactored to ListOfCaseDbo

--- src/data/database/CaseDbo.kt
+++ src/data/database/CaseDbo.kt
@@ -6,14 +6,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class CaseDbo(
-        val id: Int,
-        val caseAccess:String,
-        val name: String,
-        val releaseDate: String,
-        val dropStatus: String,
-        val lowestPrice: Double,
-        val volume: Int,
-        val medianPrice: Double,
-        val imageUrl: String,
-        val description: String
+    val id: Int = 0,
+    val caseAccess: String,
+    val name: String,
+    val releaseDate: String,
+    val dropStatus: String,
+    val lowestPrice: Double,
+    val volume: Int,
+    val medianPrice: Double,
+    val imageUrl: String,
+    val description: String
 )
