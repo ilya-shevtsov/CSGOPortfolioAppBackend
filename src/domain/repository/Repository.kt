50886a7da6a7b79aa8906179abb165dc514679c8
ilya-shevtsov <Dayzilya@gmail.com>
2021-04-11 18:32:53 +0300

    Added: - flow

--- src/domain/repository/Repository.kt
+++ src/domain/repository/Repository.kt
@@ -6,8 +6,9 @@ import data.api.ApiTools
 import data.database.CaseDatabase
 import data.database.CaseDbo
 import domain.model.CaseDto
-import okhttp3.internal.wait
-import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import io.ktor.utils.io.errors.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.update
@@ -27,34 +28,44 @@ class Repository {
         )
     }
 
-
-
-    suspend fun getMarketOverview(caseName: String): SimpleCaseDto {
-        val simpleResponse = ApiTools.getApiService()
+    suspend fun getMarketOverview(caseName: String): Flow<SimpleCaseDto> = flow {
+        val response = ApiTools.getApiService()
             .getCase(
                 appId = 730,
                 currency = 5,
                 caseName = caseName
             )
-            return CaseDataResponseMapper.map(simpleResponse, caseName)
+        val simpleCaseDto = CaseDataResponseMapper.map(response, caseName)
+        emit(simpleCaseDto)
+    }.retryWhen { cause, _ ->
+        if (cause is IOException) {
+            delay(60000)
+            true
+        } else {
+            false
+        }
     }
 
     suspend fun updateInfo() {
-        val caseList = transaction {
-            CaseDatabase.selectAll().map { CaseDatabase.toCaseDbo(it) }
-        }
-
+        val caseList = getCaseList()
         caseList.forEach { case ->
-            val marketOverview = getMarketOverview(case.caseAccess)
-
-            transaction {
-                CaseDatabase.update({ CaseDatabase.id eq case.id }) { caseDatabase ->
-                    caseDatabase[lowestPrice] = marketOverview.lowestPrice
-                    caseDatabase[volume] = marketOverview.volume
-                    caseDatabase[medianPrice] = marketOverview.medianPrice
+            getMarketOverview(case.caseAccess)
+                .catch { println("Error") }
+                .collect { simpleCaseDto ->
+                transaction {
+                    CaseDatabase.update({ CaseDatabase.id eq case.id }) { caseDatabase ->
+                        caseDatabase[lowestPrice] = simpleCaseDto.lowestPrice
+                        caseDatabase[volume] = simpleCaseDto.volume
+                        caseDatabase[medianPrice] = simpleCaseDto.medianPrice
+                    }
                 }
             }
         }
     }
-}
 
+    fun getCaseList(): List<CaseDbo> {
+        return transaction {
+            CaseDatabase.selectAll().map { CaseDatabase.toCaseDbo(it) }
+        }
+    }
+}
