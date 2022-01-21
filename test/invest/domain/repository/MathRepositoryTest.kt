cb39253b37851521b1ac8756f699fdbb982ce30c
ilya-shevtsov <Dayzilya@gmail.com>
2022-01-21 16:22:28 +0300

    Added: - test template

---
+++ test/invest/domain/repository/MathRepositoryTest.kt
@@ -0,0 +1,15 @@
+package invest.domain.repository
+
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+
+class MathRepositoryTest {
+    private val mathRepository = MathRepository()
+
+
+    @Test
+    fun `kkk`() {
+        assertTrue(true)
+    }
+}
