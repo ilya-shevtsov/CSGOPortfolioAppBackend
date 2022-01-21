fb3fae20b028cbf8fd62a82d75b45eba026b950a
ilya-shevtsov <Dayzilya@gmail.com>
2022-01-21 16:34:54 +0300

    Added: - test for getAverageReturn P and C

--- test/invest/domain/repository/MathRepositoryTest.kt
+++ test/invest/domain/repository/MathRepositoryTest.kt
@@ -1,7 +1,7 @@
 package invest.domain.repository
 
 import org.junit.Test
-import org.junit.jupiter.api.Assertions.assertTrue
+import kotlin.test.assertEquals
 
 
 class MathRepositoryTest {
@@ -9,7 +9,40 @@ class MathRepositoryTest {
 
 
     @Test
-    fun `kkk`() {
-        assertTrue(true)
+    fun `getAverageReturn P and C when price gain is 100 RUB`() {
+
+        val priceList = listOf(100.0,200.0)
+        val averageReturnTypeP = 1
+        val averageReturnTypeC = 2
+
+        val getAverageReturnP = mathRepository.getAverageReturn(
+            priceList = priceList,
+            averageReturnType = averageReturnTypeP
+        )
+        val getAverageReturnC = mathRepository.getAverageReturn(
+            priceList = priceList,
+            averageReturnType = averageReturnTypeC
+        )
+        assertEquals(100.0,getAverageReturnP)
+        assertEquals(100.0,getAverageReturnC)
+    }
+
+    @Test
+    fun `getAverageReturn P and C when price gain is -100 RUB`() {
+
+        val priceList = listOf(200.0,100.0)
+        val averageReturnTypeP = 1
+        val averageReturnTypeC = 2
+
+        val getAverageReturnP = mathRepository.getAverageReturn(
+            priceList = priceList,
+            averageReturnType = averageReturnTypeP
+        )
+        val getAverageReturnC = mathRepository.getAverageReturn(
+            priceList = priceList,
+            averageReturnType = averageReturnTypeC
+        )
+        assertEquals(-50.0,getAverageReturnP)
+        assertEquals(-100.0,getAverageReturnC)
     }
 }
